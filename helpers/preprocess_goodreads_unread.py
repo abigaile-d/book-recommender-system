fa13e6ae34ba051fcbb1e23c856bd5e30079c45b
abby <abigaile-d@users.noreply.github.com>
2023-04-27 18:45:19 +0300

    updated processing of unread books

--- helpers/preprocess_goodreads_unread.py
+++ helpers/preprocess_goodreads_unread.py
@@ -19,35 +19,70 @@ file_ratings = os.path.join(root, 'book_ratings_{}.csv')
 file_unreads = os.path.join(root, 'book_unreads_{}.csv')
 file_titles = os.path.join(root, 'book_titles.csv')
 
-print("Loading json files to dataframe...")
+print("Loading ratings csv to dataframe...")
+df_list = []
+for mode in ('train', 'test'):
+    print("Processing:", mode)
+    # load list of books that the user has already read
+    tmp_df = pd.read_csv(file_ratings.format(mode), sep=',', header=0, quotechar='"')
+    tmp_df['mode'] = mode
+    df_list.append(tmp_df)
+all_ratings_df = pd.concat(df_list, ignore_index=True)
+all_ratings_df.set_index(['encoded_user_id'], inplace=True)
+df_list = None
+print(all_ratings_df)
 
+# load list of all books
+titles_df = pd.read_csv(file_titles, sep=',', header=0, quotechar='"')
+titles_df.set_index(['encoded_book_id'], inplace=True)
+
+print("Creating list of unread books...")
 for mode in ('train', 'test'):
     print("Processing:", mode)
 
     # load list of books that the user has already read
-    ratings_df = pd.read_csv(file_ratings.format(mode), sep=',', header=0, quotechar='"')
-    ratings_df.set_index(['encoded_user_id'], inplace=True)
+    # ratings_df = pd.read_csv(file_ratings.format(mode), sep=',', header=0, quotechar='"')
+    # ratings_df.set_index(['encoded_user_id'], inplace=True)
+    ratings_df = all_ratings_df.loc[all_ratings_df['mode'] == mode]
 
-    # load list of all books
-    titles_df = pd.read_csv(file_titles, sep=',', header=0, quotechar='"')
-    titles_df.set_index(['encoded_book_id'], inplace=True)
+    if mode == 'test':
+        unreads_df_train = unreads_df[['encoded_user_id', 'encoded_book_id']].copy()
+        unreads_df_train.set_index('encoded_user_id', inplace=True)
 
+    unreads_df = ratings_df.copy()
+    unreads_df[['book_id', 'encoded_book_id', 'rating']] = 0
+    unreads_df['datetime_read'] = None
+    print(ratings_df)
+    print(unreads_df)
+    
     # find list of all books that the user has not read, 
     # and get a random sample with size the same as the books that the user has already read
     for user_id in ratings_df.index.unique():
-        read_books = ratings_df.loc[user_id, 'encoded_book_id']
-        if not isinstance(read_books, pd.Series):
-            read_books = [read_books]
+        # if train, get list of previously read books from train.csv
+        if mode == 'train':
+            read_books = ratings_df.loc[user_id, 'encoded_book_id']
+            if not isinstance(read_books, pd.Series):
+                read_books = [read_books]
+            sample_size = len(read_books)
+        # if test, get list of all previous read books, and all unread books not included in training
+        else:
+            read_books = all_ratings_df.loc[user_id, 'encoded_book_id'].tolist()
+            if isinstance(read_books, int):
+                read_books = [read_books]
+            unread_books_train = unreads_df_train.loc[user_id, 'encoded_book_id'].tolist()
+            if isinstance(unread_books_train, int):
+                unread_books_train = [unread_books_train]
+            sample_size = len(read_books) - len(unread_books_train)
+            read_books.extend(unread_books_train)
+
         unread_books = titles_df.loc[~titles_df.index.isin(read_books)]
-        unread_books = unread_books.sample(n=len(read_books))
-        # print(unread_books)
+        unread_books = unread_books.sample(n=sample_size)
         
-        ratings_df.loc[user_id, ['encoded_book_id']] = unread_books.index.values
-        ratings_df.loc[user_id, ['book_id']] = unread_books['book_id'].values
-        ratings_df.loc[user_id, ['rating']] = 0.0
+        unreads_df.loc[user_id, ['encoded_book_id']] = unread_books.index.values
+        unreads_df.loc[user_id, ['book_id']] = unread_books['book_id'].values
 
     # save to csv
-    print(ratings_df)
+    print(unreads_df)
     print("Saving to:", file_unreads.format(mode))
-    ratings_df.reset_index(inplace=True)
-    ratings_df[['book_id', 'user_id', 'encoded_book_id', 'encoded_user_id', 'rating', 'datetime_read']].to_csv(file_unreads.format(mode), sep=',', header=True, index=False)
+    unreads_df.reset_index(inplace=True)
+    unreads_df[['book_id', 'user_id', 'encoded_book_id', 'encoded_user_id', 'rating', 'datetime_read']].to_csv(file_unreads.format(mode), sep=',', header=True, index=False)
